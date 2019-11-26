46f187cf2093c3674b2ce1aeed9c41e841fce3e3
Kamila RB <kama.rakoczy@gmail.com>
2019-11-26 20:27:06 +0100

    poprawki - import

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { PagesView } from "./pages/index.js";
 import { UsersView } from "./users/index.js";
 import { TodosView } from "./todos/index.js";
 import { MateuszPageView } from "./pages/mateusz.js";
+import { KamilaPageView } from "./pages/kamila.js";
 import { Router } from './lib/router.js'
 import { FrontPageView } from './lib/frontpageview.js'
 import { NavigationView } from "./nav/index.js";
